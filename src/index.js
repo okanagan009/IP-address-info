9fbdc334762ba19c71bd01bb87740e12c96b5027
okanagan009 <serezharostov161@gmail.com>
2025-01-13 02:27:00 +0800

    function for obtaining data by IP is implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
 
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+    fetch(`http://ipwho.is/${ipInput.value}?lang=ru`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+function handleKey(e) {
+    if (e.key === 'Enter') {
+        getData();
+    }
+}  
