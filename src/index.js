270e073a4d11449f218e29ac0a88f5ccce1d3f50
okanagan009 <serezharostov161@gmail.com>
2025-01-17 01:11:30 +0800

    added a map

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ ipInput.addEventListener('keydown', handleKey);
 function getData() {
     if (validatIp(ipInput.value)) {
         fetch(`http://ipwho.is/${ipInput.value}?lang=ru`)
-        .then(response => response.json())
-        .then(data => setInfo(data))
+            .then(response => response.json())
+            .then(data => setInfo(data))
     }
 }
 
@@ -23,7 +23,7 @@ function handleKey(e) {
     if (e.key === 'Enter') {
         getData();
     }
-}  
+}
 
 function setInfo(mapData) {
     console.log(mapData);
@@ -32,3 +32,27 @@ function setInfo(mapData) {
     timezoneInfo.innerText = " " + mapData.timezone.utc;
     ispInfo.innerText = mapData.connection.isp;
 }
+
+ymaps.ready(() => {
+    // Инициализация карты
+    const map = new ymaps.Map('map', {
+        center: [55.751244, 37.618423], // Координаты центра карты (Москва)
+        zoom: 10, // Уровень масштабирования
+    });
+
+    // Создание метки
+    const placemark = new ymaps.Placemark(
+        [55.751244, 37.618423], // Координаты метки
+    );
+
+    // Добавление метки на карту
+    map.geoObjects.add(placemark);
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты
+});
